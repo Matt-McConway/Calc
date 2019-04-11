2d7e0727ba7e6ce531edba7a555b9a506d7637a6
Matthew McConway <mmcc800@aucklanduni.ac.nz>
2019-04-11 21:07:23 +1200

    Initial commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import { StyleSheet, Text, View} from 'react-native';
+
+
+export default class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  
+});
