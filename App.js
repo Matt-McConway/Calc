8e156c1be5d4e9d9a27c88083cd408502753e0ac
Matthew McConway <mmcc800@aucklanduni.ac.nz>
2019-04-11 21:29:08 +1200

    UI Layout Setup

--- App.js
+++ App.js
@@ -12,6 +12,12 @@ export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
+        <View style={styles.result}></View>
+        <View style={styles.calculation}></View>
+        <View style={styles.buttons}>
+          <View style={styles.numbers}></View>
+          <View style={styles.operations}></View>
+        </View>
         
       </View>
     );
@@ -19,5 +25,27 @@ export default class App extends Component {
 }
 
 const styles = StyleSheet.create({
-  
+  container: {
+    flex: 1
+  },
+  result: {
+    flex: 2,
+    backgroundColor: "red"
+  },
+  calculation: {
+    flex: 1,
+    backgroundColor: "green"
+  },
+  buttons: {
+    flex: 7,
+    flexDirection: "row"
+  },
+  numbers: {
+    flex: 3,
+    backgroundColor: "yellow"
+  },
+  operations: {
+    flex: 1,
+    backgroundColor: "black"
+  }
 });
